2794bc31b9f2d2675c12426cd9e3ca86b148edc1
Kaike Bartolomeu <102388705+Kaike2003@users.noreply.github.com>
2024-09-17 00:17:01 +0100

    alteracao de link da rede social

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -165,7 +165,7 @@ export default function Page() {
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                 Quer conversar? Apenas me mande uma mensagem{" "}
                 <Link
-                  href={DATA.contact.social.X.url}
+                  href={DATA.contact.social.LinkedIn.url}
                   className="text-blue-500 hover:underline"
                 >
                   com uma pergunta direta no linkedin
